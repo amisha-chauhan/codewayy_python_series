9052b4e80c6573d900342992ce4911dedb950568
Amisha <amishachauhan48@gmail.com>
2020-06-23 17:35:29 +0530

    added task-1

---
+++ Python-Task1/INT-FLOAT.py
@@ -0,0 +1,40 @@
+#TASK-1
+#PART-1
+
+#Working with Integers
+
+num_1 = 10
+num_2 = 30
+sum = 0
+product = 0
+
+sum = num_1 + num_2
+product = num_1 * num_2
+division = num_2 / num_1
+remainder = num_2 % num_1
+square = num_1 ** 3
+
+print("The sum is= ", sum)
+print("The product is= ", product)
+print("The division is= ", division)
+print("The remainder is= ", remainder)
+print("Square of the num_1= ",square)
+
+
+#Working with Float
+
+num_1 = 10.5
+num_2 = 5.2
+sum = 0
+product = 0
+
+sum = num_1 + num_2
+product = num_1 * num_2
+
+print("The sum is= ", sum)
+print("The product is= ", product)
+print("Sum after rounding off= ",round(sum))
+print("Product after rounding of to first decimal= ", round(product,1))
+print(" ")
+
+
---
+++ Python-Task1/strings.py
@@ -0,0 +1,47 @@
+#TASK-1
+#PART-2
+
+#Name 
+firstName = 'Amisha'
+lastName = 'Chauhan'
+
+#College name with address
+collegeName = "Amity University"
+address = "Sec-125, Gautam Buddh Nagar, Noida,Uttar Pradesh"
+
+#Using string concatenation
+fullName = firstName + ' ' + lastName
+collegeDetails = collegeName + ', ' + address
+print(fullName)
+print(collegeDetails)
+
+#Marks of five subjects(declaring variables)
+Hindi = 85
+English = 90
+Maths = 95
+Chemistry = 94
+Physics = 87
+print("\nMarks of five subjects: ")
+print("\n")
+print("1.Hindi= ", Hindi)
+print("2.English= ",English)
+print("3.Maths= ", Maths)
+print("4.Chemistry= ", Chemistry)
+print("5.Physics= ", Physics)
+
+#calculating total marks and percentage
+totalMarks = Hindi + English + Maths + Chemistry + Physics
+print("\nTotal Marks = ", totalMarks)
+
+percentage = (totalMarks/500)*100
+print("\nPercentage of marks= ", percentage)
+
+
+
+
+
+
+
+
+
+
